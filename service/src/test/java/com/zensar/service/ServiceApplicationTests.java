82c28ea60243d28d23eb2fb9e85bab75561f0f1b
sharadrajore <sharad.rajore@gmail.com>
2020-04-30 12:26:27 +0530

    Initial Commit

---
+++ service/src/test/java/com/zensar/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
